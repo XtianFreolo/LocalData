917690e13f3369563e44ab5e498ee5483eda49a9
XtianFreolo <F60@umbc.edu>
2025-05-02 14:05:10 -0400

    updated noteColor

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-document.addEventListener("DOMContentLoaded", function ()
-{
+document.addEventListener("DOMContentLoaded", function () {
 	const noteContainer = document.getElementById("note-container");
 	const newNoteButton = document.getElementById("new-note-button");
 	const colorForm = document.getElementById("color-form");
@@ -7,8 +6,11 @@ document.addEventListener("DOMContentLoaded", function ()
 
 	// TODO: Load the note color from the local storage.
 	// If no color is found, set the default color to white.
-	localStorage.setItem("noteColor", "white");
-	let noteColor = localStorage.getItem("noteColor") || "white";
+	let noteColor = localStorage.getItem("noteColor");
+	if (!noteColor) {
+		noteColor = "white";
+		localStorage.set("noteColor", noteColor);
+	}
 
 
 	// TODO: Load the note ID counter from the local storage.
@@ -16,21 +18,21 @@ document.addEventListener("DOMContentLoaded", function ()
 	localStorage.setItem("noteIdCounter", noteIdCounter);
 
 	// TODO: Load the notes from the local storage. // using JSON.parse
-	
-	const savedNotes = JSON.parse(localStorage.getItem("notes")) || []; 
+
+	const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
 	for (const noteData of savedNotes) {
 		const note = document.createElement("textarea");
 		note.setAttribute("data-note-id", noteData.id);
 		note.value = noteData.content;
 		note.className = "note";
 		note.style.backgroundColor = noteColor;
-		noteContainer.appendChild(note); 
+		noteContainer.appendChild(note);
 	}
 
 
 
-	function addNewNote ()
-	{
+	function addNewNote() {
 		const id = noteIdCounter;
 		const content = `Note ${id}`;
 
@@ -47,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function ()
 		// TODO: Add new note to the saved notes in the local storage.
 
 		const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
-		savedNotes.push({ id: id, content: content }); 
+		savedNotes.push({ id: id, content: content });
 
 		localStorage.setItem("notes", JSON.stringify(savedNotes));
 		localStorage.setItem("noteIdCounter", noteIdCounter);
@@ -56,15 +58,13 @@ document.addEventListener("DOMContentLoaded", function ()
 
 	}
 
-	colorForm.addEventListener("submit", function (event)
-	{
+	colorForm.addEventListener("submit", function (event) {
 		event.preventDefault(); // Prevents the default event.
 
 		const newColor = colorInput.value.trim();  // Removes whitespaces.
 
 		const notes = document.querySelectorAll(".note");
-		for (const note of notes)
-		{
+		for (const note of notes) {
 			note.style.backgroundColor = newColor;
 		}
 
@@ -75,40 +75,32 @@ document.addEventListener("DOMContentLoaded", function ()
 		// TODO: Update the note color in the local storage.
 	});
 
-	newNoteButton.addEventListener("click", function ()
-	{
+	newNoteButton.addEventListener("click", function () {
 		addNewNote();
 	});
 
-	document.addEventListener("dblclick", function (event)
-	{
-		if (event.target.classList.contains("note"))
-		{
+	document.addEventListener("dblclick", function (event) {
+		if (event.target.classList.contains("note")) {
 			event.target.remove(); // Removes the clicked note.
 
 			// TODO: Delete the note from the saved notes in the local storage.
 		}
 	});
 
-	noteContainer.addEventListener("blur", function (event)
-	{
-		if (event.target.classList.contains("note"))
-		{
+	noteContainer.addEventListener("blur", function (event) {
+		if (event.target.classList.contains("note")) {
 			// TODO: Update the note from the saved notes in the local storage.
 		}
 	}, true);
 
-	window.addEventListener("keydown", function (event)
-	{
+	window.addEventListener("keydown", function (event) {
 		/* Ignores key presses made for color and note content inputs. */
-		if (event.target.id === "color-input" || event.target.type === "textarea")
-		{
+		if (event.target.id === "color-input" || event.target.type === "textarea") {
 			return;
 		}
 
 		/* Adds a new note when the "n" key is pressed. */
-		if (event.key === "n" || event.key === "N")
-		{
+		if (event.key === "n" || event.key === "N") {
 			addNewNote(); // Adds a new note.
 		}
 	});
